227ea7a76631309fa9cf6e7aeb8c95951f62075b
SARA RAMIREZ <sara_ramatis@hotmail.com>
2024-01-02 23:15:26 -0600

    conecta con la Api desde el modulo Header

--- MC-laboratoria/src/app/header/header.component.ts
+++ MC-laboratoria/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APImovieService } from '../service/apimovie.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  data: any =  {};
+
+  constructor(private apiService: APImovieService) { }
 
   ngOnInit(): void {
+    this.llenarData()
   }
-
+llenarData(){
+  this.apiService.getData().subscribe(data => {
+    this.data = data;
+    console.log(this.data);
+    
+  })
+}
 }
--- MC-laboratoria/src/app/service/apimovie.service.ts
+++ MC-laboratoria/src/app/service/apimovie.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class APImovieService {
 
-  constructor() { }
+  private urlApi= 'https://api.themoviedb.org/3/movie/changes?page=1';
+  private apiKey= 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmQ3OTg3ODcxYWZjOGY0ZjBhNmNhODhmYmMwMWYxMCIsInN1YiI6IjY1N2E3ZjBkMzVhNjFlMDEzYWMyYmM2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fHZyKVfaggDV8RJxFFhF5dEJExFddVoqq8ZXQXWXrAc';
+
+  constructor( private http: HttpClient) { } 
+
+  public getData() : Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.apiKey}`,
+    });
+    //const keyMovie = new HttpHeaders().set('Authorization', `Bearer ${this.apiKey}`);
+    return this.http.get<any>(`${this.urlApi}/https://api.themoviedb.org/3/movie/changes?page=1`,  {headers});
+  }
+  
 }
