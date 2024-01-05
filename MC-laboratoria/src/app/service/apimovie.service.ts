5804326c3819a342e63d21572f807fc5172f1458
SARA RAMIREZ <sara_ramatis@hotmail.com>
2024-01-04 22:24:12 -0600

    css de header

--- MC-laboratoria/src/app/service/apimovie.service.ts
+++ MC-laboratoria/src/app/service/apimovie.service.ts
@@ -10,6 +10,11 @@ export class APImovieService {
   private urlApi= 'https://api.themoviedb.org/3/discover/movie';
   private apiKey= 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmQ3OTg3ODcxYWZjOGY0ZjBhNmNhODhmYmMwMWYxMCIsInN1YiI6IjY1N2E3ZjBkMzVhNjFlMDEzYWMyYmM2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fHZyKVfaggDV8RJxFFhF5dEJExFddVoqq8ZXQXWXrAc';
 
+  // discobery https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc 
+  // filtro https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
+  // input https://api.themoviedb.org/3/movie/movie_id?language=en-US
+
+
   constructor( private http: HttpClient) { } 
 
   public getData() : Observable<any> {
