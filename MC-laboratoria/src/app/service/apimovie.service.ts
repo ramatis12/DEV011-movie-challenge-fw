32af44f4160b837af94bfc80faee29d14df72801
Sara Ramirez <sara_ramatis@hotmail.com>
2024-01-15 17:00:23 -0600

    lunes 15

--- MC-laboratoria/src/app/service/apimovie.service.ts
+++ MC-laboratoria/src/app/service/apimovie.service.ts
@@ -8,7 +8,10 @@ import { Observable } from 'rxjs';
 export class APImovieService {
 
   private urlApi= 'https://api.themoviedb.org/3/discover/movie';
+  private urlGeneros = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+  private apiKeyG = 'dfd7987871afc8f4f0a6ca88fbc01f10';
   private apiKey= 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmQ3OTg3ODcxYWZjOGY0ZjBhNmNhODhmYmMwMWYxMCIsInN1YiI6IjY1N2E3ZjBkMzVhNjFlMDEzYWMyYmM2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fHZyKVfaggDV8RJxFFhF5dEJExFddVoqq8ZXQXWXrAc';
+
 
   // discobery https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc 
   // filtro https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
@@ -24,4 +27,11 @@ export class APImovieService {
     return this.http.get<any>(this.urlApi, { headers: headers });
   }
   
+  public getGenery() : Observable<any> {
+    const headersG = new HttpHeaders({
+      'Authorization': `Bearer ${this.apiKeyG}`,
+    });
+    return this.http.get<any>(this.urlGeneros, { headers: headersG });
+  } 
+
 }
